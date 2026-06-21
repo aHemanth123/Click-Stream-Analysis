ba7bbbd00479a8056971695a0ac054c8cfcd61f7
A . Hemanth Sai Manikanta <133625674+aHemanth123@users.noreply.github.com>
2026-06-21 18:04:33 +0530

    Add analytics queries for clickstream_events

--- SQL/analytics_queries.sql
+++ SQL/analytics_queries.sql
@@ -1,16 +1,21 @@
+1
 SELECT url, COUNT(*) AS visits
 FROM clickstream_events
 GROUP BY url;
 
+2
 SELECT error_code, COUNT(*) AS total
 FROM clickstream_events
 GROUP BY error_code;
 
+3
 SELECT
     SUM(transaction) AS transactions,
     COUNT(*) AS total_events
 FROM clickstream_events;
 
+
+4
 SELECT
     AVG(page_load_time) AS avg_load_time
 FROM clickstream_events;
